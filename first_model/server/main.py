a56a3dc92594a25bdd9a95fd1ddb376f224ca36a
Joshua Wong Desktop <zhuoyijoshua@gmail.com>
2025-08-30 23:06:53 +0800

    fixed string object

--- first_model/server/main.py
+++ first_model/server/main.py
@@ -19,7 +19,7 @@ def audit_project(project_id: int):
 
         evidence_dict = json.loads(audit_response[0]["evidence"])
         clean_evidence_dict = {f"{doc_ids[0]}": evidence_dict["prd"], f"{doc_ids[1]}": evidence_dict["tdd"]}
-
+        
         issue_id = database.create_issue(audit_id=audit_id, issue_description=audit_response[0]["reasoning"], ent_id=int(law_used[0]) if law_used else -1, status="open", evidence=clean_evidence_dict, qn=audit_response[0]["clarification_question"])
         conv_id = database.create_conversation(audit_id=audit_id, issue_id=issue_id)
         start_convo = database.send_first_message(conv_id=conv_id, role="ai", content=audit_response[0]["clarification_question"])
